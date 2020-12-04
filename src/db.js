bcf376148f6cbedca343dbb6740f6c759d0d6d42
Lucas Passos <lucas.depassos@yahoo.com.br>
2020-12-04 11:51:04 -0300

    client-creating

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
     password:"7addb767935bb796ff99d2da1d76a5bd91e05ff26f9c08fb2e386111b840f8be",
     host:"ec2-52-22-216-69.compute-1.amazonaws.com",
     port:5432,
-    database:"d5rkgu5oq6eqip",
+    database:"d5rkgu5oq6eqip", // Aprova-fácil-Staging
     ssl: true
 })
   
